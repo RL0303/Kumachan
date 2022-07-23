22d23826dc18d02e9136d6fd46d9f09a86a0a0c2
Robert <linrobert0303@gmail.com>
2022-07-23 13:49:08 +0800

    Initial Commit

---
+++ Kumachan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kumachan
+//
+//  Created by 林辰澤 on 2022/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
